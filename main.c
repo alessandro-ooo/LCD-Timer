2e08536a8e0b2122056c8221c427db384ef23a73
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2023-12-18 22:47:26 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,80 @@
+
+#include <Adafruit_LiquidCrystal.h>
+
+const int increaserBtnPin = 4;
+const int decreaserBtnPin = 2;
+const int handleBtnPin = 7;
+
+int increaserBtnState = LOW;
+int decreaseBtnState = LOW;
+int handleBtnState = LOW;
+int seconds = 5;
+bool isCounting = false;
+
+Adafruit_LiquidCrystal timerLCD(0);
+
+
+void setup()
+{
+	pinMode(increaserBtnPin, INPUT);
+  	pinMode(decreaserBtnPin, INPUT);
+    pinMode(handleBtnPin, INPUT);
+  
+  	timerLCD.begin(16, 2);
+  	timerLCD.setCursor(7, 8);
+  	printSeconds(seconds);
+}
+
+void printSeconds (int number) {
+    timerLCD.clear();
+    timerLCD.setCursor(7, 8);
+    timerLCD.print(number); 
+    return;
+}
+
+void tickLCD(int currentSeconds) {
+  	
+  	for(int sec = currentSeconds; sec >= 0; sec--)
+    {
+        delay(1000);
+        seconds -= 1;
+
+        printSeconds(seconds);
+    }
+  
+  	isCounting = false;
+  	seconds = 5;
+  	printSeconds(seconds);
+}
+
+void loop() {
+    if(digitalRead(handleBtnPin) == HIGH) {
+        delay(50);
+        switch(isCounting) {
+            case false: {
+                tickLCD(seconds);
+                isCounting = true;
+                break;
+            }
+
+            case true: {
+                isCounting = false;
+                break;
+            }
+        }
+    }
+
+	if(digitalRead(increaserBtnPin) == HIGH) {
+        delay(50);
+		seconds += 5;
+
+        printSeconds(seconds);
+  	}
+  
+	if(digitalRead(decreaserBtnPin) == HIGH) {
+        delay(50);
+		seconds = (seconds - 5 <= 5) ? seconds = 5 : seconds -= 5;
+
+        printSeconds(seconds);
+  	}
+}
